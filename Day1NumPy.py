4b5252a8b074bf66fe22713df4c4ee2368cb11d3
Daranton <104932961+Daranton@users.noreply.github.com>
2024-12-09 16:02:02 +0000

    Add files via upload

--- Day1NumPy.py
+++ Day1NumPy.py
@@ -1,4 +1,14 @@
 from numpy import loadtxt, sort
+from collections import defaultdict
 
-A, B = sort(loadtxt('/home/incorporated/Documents/IT/Python/AdventCode2024/input.txt').T)
+A, B = sort(loadtxt('/home/incorporated/Documents/IT/Python/AdventCode2024/Day1input.txt').T)
 print(sum(abs(A - B)))
+
+sum = 0
+similiarity_score = defaultdict(int)
+for x in B:
+    similiarity_score[x] += 1
+for x in A:
+    sum += x * similiarity_score[x]
+
+print(sum)
